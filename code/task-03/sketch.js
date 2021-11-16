bdfc0d57f6c5df79c806daa1954d2cd5c42f30ac
emmaluisa <elb.sp@gmx.de>
2021-11-16 19:32:26 +0100

    Übung3
    Übung 3

---
+++ code/task-03/sketch.js
@@ -0,0 +1,72 @@
+
+
+
+let colorValue="red"
+
+function setup() 
+{
+  createCanvas(400, 400);
+}
+
+function draw() {
+  
+  background (colorValue);
+  fill("blue");
+
+  let t = map(mouseX, 0, width, -10, 10);
+  curveTightness(t);
+
+  for(let i = 0; i < 50; i += 1) {
+    for(let y = 0; y < 10; y +=1)
+      circle(10 * i, 300 * y, 200);}
+  
+      
+      
+     
+      
+    beginShape();
+
+    curveVertex(200, 400);
+    curveVertex(100, 600);
+    curveVertex(200, 360);
+    curveVertex(250, 230);
+    curveVertex(200, 50);
+    curveVertex(0, 150);
+    curveVertex(400, 200);
+    curveVertex(200, 300);
+    curveVertex(10, 600);
+   
+       
+    endShape(CLOSE);
+
+
+          
+
+
+         
+
+
+         
+        }
+          
+          
+
+
+
+
+
+   
+
+   
+   
+     
+    
+      
+  
+
+ 
+
+ 
+  
+    
+  
